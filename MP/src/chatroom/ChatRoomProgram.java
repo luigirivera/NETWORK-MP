68ebb880d86f9d3e4c39e9a142c81213d0b81482
Luigi Rivera <luigiriveraiv@gmail.com>
2018-02-19 19:14:21 +0800

    New shit

---
+++ MP/src/chatroom/ChatRoomProgram.java
@@ -0,0 +1,10 @@
+package chatroom;
+
+public class ChatRoomProgram {
+
+	public void start() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ MP/src/chatroom/Driver.java
@@ -0,0 +1,9 @@
+package chatroom;
+
+public class Driver {
+
+	public static void main(String[] args) {
+		ChatRoomProgram crp = new ChatRoomProgram();
+		crp.start();
+	}
+}

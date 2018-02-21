a93c7118f72283c61a76eb030beb0e3ed49ed267
Luigi Rivera <luigiriveraiv@gmail.com>
2018-02-21 22:11:12 +0800

    Making logs window

--- MP/src/chatroomView/ServerView.java
+++ MP/src/chatroomView/ServerView.java
@@ -9,6 +9,25 @@ public class ServerView extends JFrame {
 	private Server model;
 	
 	public ServerView(Server server) {
+		super("Server Logs");
 		model = server;
+		
+		this.init();
+		setSize(400,1000);
+		setVisible(true);
+	}
+	
+	private void init() {
+		JPanel panel = new JPanel();
+		JScrollPane laScroll = new JScrollPane();
+		JTextArea logsArea = new JTextArea();
+		
+		laScroll.setViewportView(logsArea);
+		logsArea.setSize(500, 519);
+		logsArea.setRows(59);
+		logsArea.setColumns(33);
+		panel.add(laScroll);
+		
+		add(panel);
 	}
 }

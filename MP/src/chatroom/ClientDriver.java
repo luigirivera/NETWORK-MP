0fe3ac3cc6a13b01ea1eebaecef30f5e841a143b
Luigi Rivera <luigiriveraiv@gmail.com>
2018-02-21 20:13:10 +0800

    Changes in packages

--- MP/src/chatroom/ClientDriver.java
+++ MP/src/chatroom/ClientDriver.java
@@ -1,5 +1,7 @@
 package chatroom;
 
+import chatroomModel.Client;
+
 public class ClientDriver {
 
 	public static void main(String[] args) {
--- MP/src/chatroom/ServerDriver.java
+++ MP/src/chatroom/ServerDriver.java
@@ -1,5 +1,7 @@
 package chatroom;
 
+import chatroomModel.Server;
+
 public class ServerDriver {
 
 	public static void main(String[] args) {

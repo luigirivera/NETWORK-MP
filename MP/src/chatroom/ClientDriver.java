d8c60411dd7ad05d53e9feb4998f993ade056dd0
Luigi Rivera <luigiriveraiv@gmail.com>
2018-02-22 22:40:48 +0800

    fixed Driver

--- MP/src/chatroom/ClientDriver.java
+++ MP/src/chatroom/ClientDriver.java
@@ -6,7 +6,6 @@ public class ClientDriver {
 
 	public static void main(String[] args) {
 		Client client = new Client();
-		client.init();
 		ClientView cView = new ClientView(client);
 		ClientController cController = new ClientController(client, cView);
 		cController.init();
